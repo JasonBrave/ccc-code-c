8b6ac404ab1030492f1c751de4ec9bc38a8f136a
JasonBrave <40355221+JasonBrave@users.noreply.github.com>
2018-12-31 10:50:32 -0500

    add 2017j1

---
+++ 2017j1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int x,y;
+    scanf("%d\n",&x);
+    scanf("%d",&y);
+    if((x>0)&&(y>0)){
+        puts("1");
+    }else if((x<0)&&(y>0)){
+        puts("2");
+    }else if((x<0)&&(y<0)){
+        puts("3");
+    }else{
+        puts("4");
+    }
+    return 0;
+}
