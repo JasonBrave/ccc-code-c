c213df567cf57d5433b0db24934f99a437d0d773
JasonBrave <40355221+JasonBrave@users.noreply.github.com>
2018-12-31 14:18:40 -0500

    add 2016j1

---
+++ 2016j1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int w=0,i;
+    for(i=0;i<12;i++){
+        if(getchar()=='W'){
+            w++;
+        }
+    }
+    if((w==5)||(w==6)){
+        putchar('1');
+    }else if((w==3)||(w==4)){
+        putchar('2');
+    }else if((w==1)||(w==2)){
+        putchar('3');
+    }else{
+        puts("-1");
+    }
+    putchar('\n');
+}
