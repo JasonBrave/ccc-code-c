e64d90c3be3abb059c1de7c7fa6a003f4c9a832c
JasonBrave <40355221+JasonBrave@users.noreply.github.com>
2018-12-31 11:24:49 -0500

    add 2017j2

---
+++ 2017j2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int n,k,i,sum=0;
+    scanf("%d\n",&n);
+    scanf("%d",&k);
+    for(i=0;i<=k;i++){
+        sum+=n*pow(10,i);
+    }
+    printf("%d\n",sum);
+}
