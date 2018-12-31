36d719947de877eed56f0333c783a92b35cfa72c
JasonBrave <40355221+JasonBrave@users.noreply.github.com>
2018-12-31 12:22:08 -0500

    add 2017j3

---
+++ 2017j3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    int a,b,c,d,t,x,y;
+    scanf("%d %d\n",&a,&b);
+    scanf("%d %d\n",&c,&d);
+    scanf("%d",&t);
+    x=abs(a-b);
+    y=abs(c-d);
+    if((x+y)<t){
+        puts("Y");
+    }else{
+        puts("N");
+    }
+}
