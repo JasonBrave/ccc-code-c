afc41685c0229330f7e2ce5c9fad889d3d25b032
JasonBrave <40355221+JasonBrave@users.noreply.github.com>
2019-01-06 13:13:16 -0500

    add 2015j4

---
+++ 2015j4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int friendwaittotal[101];
+int friendpresent[101];
+int friendstatus[101];
+int main(){
+    int m,i,texttime=0;
+    int friendid;
+    char action;
+    scanf("%d\n",&m);
+    for(i=0;i<m;i++){
+        if(i==m-1){
+            scanf("%c %d",&action,&friendid);
+        }else{
+            scanf("%c %d\n",&action,&friendid);
+        }
+        if(action=='R'){
+            friendstatus[friendid]=texttime;
+            texttime++;
+            friendpresent[friendid]=1;
+        }else if(action=='S'){
+            friendwaittotal[friendid]+=texttime-friendstatus[friendid];
+            friendstatus[friendid]=0;
+            texttime++;
+        }else{
+            texttime+=friendid-1;
+        }
+    }
+    for(i=1;i<=100;i++){
+        if(friendpresent[i]){
+            if(friendstatus[i]==0){
+                if(friendwaittotal[i]!=0){
+                    printf("%d %d\n",i,friendwaittotal[i]);
+                }else{
+                    printf("%d -1\n",i);
+                }
+            }else{
+                printf("%d -1\n",i);
+            }
+        }
+    }
+}
