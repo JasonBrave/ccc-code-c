19e71278daecd3c9e873e876579ae3ecb388c0e1
JasonBrave <40355221+JasonBrave@users.noreply.github.com>
2019-01-12 11:56:15 -0500

    add 2011j1

---
+++ 2011j1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    int antennas,eyes;
+    printf("How many antennas?");
+    scanf("%d",&antennas);
+    printf("How many eyes?");
+    scanf("%d",&eyes);
+    if((antennas>=3)&&(eyes<=4)){
+        puts("TroyMartian");
+    }
+    if((antennas<=6)&&(eyes>=2)){
+        puts("VladSaturnian");
+    }
+    if((antennas<=2)&&(eyes<=3)){
+        puts("GraemeMercurian");
+    }
+}
